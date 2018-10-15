85531bc3bfdaa48e22c7282cabd8e3cef65c5a27
0xB4DF4C3D <dongho971220@gmail.com>
2018-10-15 10:57:26 +0900

    fix urls according to django 1.11 spec

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 
+from lists import views as lists_views
+
 urlpatterns = [
-    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^$', lists_views.home_page, name='home'),
     url(r'^lists/', include('lists.urls')),
 ]
